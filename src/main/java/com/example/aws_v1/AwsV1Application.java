3787f073af222ff3ad1229d970a3dfddffbe5a47
yeeeone <dpdnjs9808@gmail.com>
2024-10-16 11:52:35 +0900

    commit

---
+++ src/main/java/com/example/aws_v1/AwsV1Application.java
@@ -0,0 +1,13 @@
+package com.example.aws_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsV1Application.class, args);
+	}
+
+}
